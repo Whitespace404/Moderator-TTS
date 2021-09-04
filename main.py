bf2fe28f950d485a961d9c6b4f0f848f5bc76222
Rahul Reji <rush2asharahul@gmail.com>
2021-09-04 11:29:53 +0530

    Updated mods list

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from twitchobserver import Observer
 import pyttsx3
 
+# for the tts
 engine = pyttsx3.init()
 channel = "pawngrubber"
 
-with Observer("<username, maybe use grubberbot>", "<get oath key here: https://twitchapps.com/tmi/>") as observer:
+with Observer("username", "Oauth") as observer:
     observer.join_channel(channel)
     print("Joined channel", channel)
+    print("Press Ctrl+C to quit, you might have to close the terminal window sometimes.")
 
     while True:
         try:
@@ -15,14 +17,18 @@ with Observer("<username, maybe use grubberbot>", "<get oath key here: https://t
                     message = event.message
                     print(message)
                     
-                    mods = observer.list_moderators(channel)
+                    # TODO get list of mods instead of hard-coding mod usernames
+                    mods = ["thefuxia", "smarterchess", "streamlabs", "grubberbot", "lalizig", "paradajzcity",
+                    "duellinksguy", "thewretch2", "vietd", "poitaoforpresident", "chesscomchris", "mynameislegyon", "f0rgetaboutit",
+                    "zuname", "rakshakthetall", "drittman13", "expired_febreeze", "strance_02", "marko_sreck", "flyingseverus"]
                     
                     if event.nickname in mods:
-                        if "!announce" in message:
-                            text = message.lstrip("!announce")
+                        if "!tts" in message:
+                            text = message.lstrip("!tts")
                             engine.say(text)
                             engine.runAndWait()
                         
         except KeyboardInterrupt:
             observer.leave_channel(channel)
+            observer.stop()
             quit()
